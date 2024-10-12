3a46e7d856267ae1dcca1baa7b5f8b7f8ad8a9d7
Poyraz <140611611+Poyraz-Arsu@users.noreply.github.com>
2024-10-13 01:03:08 +0300

    complete flags
    last flag added, TOR is not integrated yet..

---
+++ scrapper.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/chromedp/chromedp"
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	var links bool
+	var html bool
+	var screenshot bool
+
+	// Define flags for extracting links, saving HTML, and taking a screenshot
+	flag.BoolVar(&links, "links", false, "Extract links from the webpage")
+	flag.BoolVar(&html, "html", false, "Save HTML content of the webpage")
+	flag.BoolVar(&screenshot, "screenshot", false, "Take a screenshot of the webpage")
+	flag.Parse()
+
+	// Check if no flags were chosen
+	if !links && !html && !screenshot {
+		fmt.Println("No flags were chosen, setting all flags for https://sibervatan.org.")
+		links = true
+		html = true
+		screenshot = true
+	}
+
+	// Check if a URL was provided; if not, use the default URL
+	url := "https://sibervatan.org/"
+	if len(flag.Args()) > 0 {
+		url = flag.Args()[0]
+	}
+
+	// Initialize a new Colly collector
+	c := colly.NewCollector()
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("An error occurred: ", err)
+	})
+
+	// Extract links
+	if links {
+		linksFile, err := os.Create("links.txt")
+		if err != nil {
+			log.Fatal("File couldn't be created: ", err)
+		}
+		defer linksFile.Close()
+
+		c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+			link := e.Attr("href")
+			if strings.HasPrefix(link, "http") {
+				_, err := linksFile.WriteString(link + "\n")
+				if err != nil {
+					log.Println("Unable to write link:", err)
+				} else {
+					fmt.Println("Link Detected:", link)
+				}
+			}
+		})
+	}
+
+	// Save HTML content
+	if html {
+		allFile, err := os.Create("HTML.txt")
+		if err != nil {
+			log.Fatal("File couldn't be created: ", err)
+		}
+		defer allFile.Close()
+
+		c.OnResponse(func(r *colly.Response) {
+			_, err := allFile.WriteString(string(r.Body))
+			if err != nil {
+				log.Println("Unable to write HTML content:", err)
+			} else {
+				fmt.Println("HTML content written to HTML.txt")
+			}
+		})
+	}
+
+	c.Visit(url)
+
+	// Capture screenshot if the flag is set
+	if screenshot {
+		if err := captureScreenshot(url); err != nil {
+			log.Fatal("Error capturing screenshot:", err)
+		}
+		fmt.Println("Screenshot captured successfully!")
+	}
+}
+
+// Screenshot func
+func captureScreenshot(url string) error {
+
+	// Chrome init
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel() // Release the browser resources when no longer needed
+
+	var screenshotBuffer []byte
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible("body", chromedp.ByQuery), // Wait until the page is fully loaded
+		chromedp.FullScreenshot(&screenshotBuffer, 90), // Take screenshot
+	)
+	if err != nil {
+		return fmt.Errorf("failed to capture screenshot: %w", err)
+	}
+
+	// Write the screenshot to an image file
+	err = os.WriteFile("screenshot.png", screenshotBuffer, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to save screenshot to file: %w", err)
+	}
+
+	return nil
+}
